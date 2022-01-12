6c95c016276452ec90c702664e5a5d0606dea35a
Westin Humble <westin.h@gmail.com>
2022-01-12 15:57:53 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    const wins = record.find(({result}) => result === "W");
+    if(wins === undefined) {
+        return undefined;
+    }
+    return wins.year
+};
+
+superbowlWin(record);
